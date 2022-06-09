11b68d4b15d508e369100181f34997f8ef043da3
corbinarnett <corbin.c.arnett@gmail.com>
2022-06-09 13:19:21 +0200

    fix parcel hot reload bug. add custom typography

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-4xl font-extrabold">Hello World</div>;
+  return (
+    <div className="flex flex-col">
+      <h1 className="">This is an H1 and now parcel works</h1>
+      <h2 className="">This is an H2</h2>
+      <h3>This is an H3</h3>
+      <h4>This is an H4</h4>
+      <p>This is a paragraph tag</p>
+    </div>
+  );
 };
-
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ["Space Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
