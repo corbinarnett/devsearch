73c6c007ea26c0ff1ad69c792deb42a7c61aee25
corbinarnett <corbin.c.arnett@gmail.com>
2022-06-09 15:21:34 +0200

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+        button: "var(--color-bg-button)",
+      },
+      textColor: {
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+        accent: "var(--color-text-accent)",
+        alert: "var(--color-text-alert)",
+        btnText: "var(--color-link-active)",
+        btnTextHover: "var(--color-link-hover)",
+      },
       fontFamily: {
         custom: ["Space Mono", "monospace"],
       },
