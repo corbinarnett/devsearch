f3ec00741fc15d5196ee692ceecfbd1458f413ed
corbinarnett <corbin.c.arnett@gmail.com>
2022-06-10 18:00:31 +0200

    update header

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,17 +1,31 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
+import Moon from "../../assets/icon-moon.svg";
+import Sun from "../../assets/icon-sun.svg";
 
 const Toggle = () => {
   const { theme, setTheme } = useContext(ThemeContext);
-  console.log({ theme });
   return (
-    <button
-      onClick={() => {
-        theme === "dark" ? setTheme("light") : setTheme("dark");
-      }}
-    >
-      {theme === "dark" ? "dark" : "light"}
-    </button>
+    <div className="flex container mx-auto justify-between max-w-[730px] max-h-[36px] mt-36">
+      <h1>devfinder</h1>
+      {theme === "dark" ? (
+        <div
+          className="flex items-center cursor-pointer font-bold text-[13px] tracking-[2.5px]"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          LIGHT
+          <img src={Sun} alt="sun icon" className="ml-4" />
+        </div>
+      ) : (
+        <div
+          className="flex items-center cursor-pointer font-bold text-[13px] tracking-[2.5px]"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          DARK
+          <img src={Moon} alt="moon icon" className="ml-4" />
+        </div>
+      )}
+    </div>
   );
 };
 
